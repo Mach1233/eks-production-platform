f66c74457c6a21dac89d1e142b3709e65380b9d6
mohamed <mohamed.mechraoui@ensi-uma.tn>
2026-02-15 00:27:30 +0100

    feat(vpc): rewrite VPC module with pure Terraform + NAT instance
    Major changes:
    - Replaced community VPC module with pure Terraform resources
    - NAT instance (t4g.micro ARM) instead of NAT gateway (~€3 vs €30/month)
    - user_data auto-configures IP forwarding + iptables masquerade
    - EIP for static IP (whitelist in MongoDB Atlas)
    - EKS discovery tags on subnets
    - Added README with usage and cost info
    
    See: docs/decisions/ADR-002-nat-instance.md

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,9 +1,41 @@
-variable "name" { type = string }
-variable "cidr" { type = string }
-variable "azs" { type = list(string) }
-variable "private_subnets" { type = list(string) }
-variable "public_subnets" { type = list(string) }
+# -----------------------------------------------------------------------------
+# VPC Module Variables
+# -----------------------------------------------------------------------------
+
+variable "name" {
+  description = "Name prefix for all VPC resources"
+  type        = string
+}
+
+variable "cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "nat_instance_type" {
+  description = "EC2 instance type for NAT instance (ARM recommended for cost)"
+  type        = string
+  default     = "t4g.micro"
+}
+
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
 }
